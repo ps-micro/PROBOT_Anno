8a9e5f270e9fbcd0fc17d4ff426f311ed56a4e1d
TSijia <tsjgithub@163.com>
2019-07-06 23:22:59 +0800

    Update probot vision pick and place

---
+++ probot_vision_pick/probot_ocr/scripts/Detect_ocr .py
@@ -0,0 +1,413 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import cv2
+import time
+import math
+import os
+import numpy as np
+import tensorflow as tf
+from math import *
+import time
+import os
+import argparse
+import re
+
+import locality_aware_nms as nms_locality
+import lanms
+
+import model
+from icdar import restore_rectangle
+
+import roslib
+import rospy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Pose
+from cv_bridge import CvBridge, CvBridgeError
+
+from probot_ocr.srv import *
+FLAGS = tf.app.flags.FLAGS
+global point1
+global point2
+global img
+global area
+global num
+global args
+
+area = 0
+num = 0
+
+class TOD(object):
+    def __init__(self):
+
+        self.graph = tf.get_default_graph()
+        self.input_images = tf.placeholder(tf.float32, shape=[None, None, None, 3], name='input_images')
+        self.global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+        self.f_score, self.f_geometry = model.model(self.input_images, is_training=False)
+
+        self.variable_averages = tf.train.ExponentialMovingAverage(0.997, self.global_step)
+        self.saver = tf.train.Saver(self.variable_averages.variables_to_restore())
+
+        self.sess = tf.InteractiveSession(config=tf.ConfigProto(allow_soft_placement=True))
+
+        global args
+        self.ckpt_state = tf.train.get_checkpoint_state(args.model_path)
+        self.model_path = os.path.join(args.model_path, os.path.basename(self.ckpt_state.model_checkpoint_path))
+        self.saver.restore(self.sess,self.model_path)
+
+        self.poseList=[]
+        self.angleList=[]
+        self.charname = []
+        self.charnum = []
+        self.com = {}
+
+    def rotate(self,img,pt1, pt2, pt3, pt4):
+        print(pt1,pt2,pt3,pt4)
+        withRect = math.sqrt((pt4[0] - pt1[0]) ** 2 + (pt4[1] - pt1[1]) ** 2) # 矩形框的宽度
+        heightRect = math.sqrt((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) **2)
+        print(withRect,heightRect)
+        #angle = acos((pt4[0] - pt1[0]) / withRect) * (180 / math.pi) # 矩形框旋转角度
+        print("acos" ,((pt1[0] - pt4[0]) / withRect))
+        angleh = math.acos((pt1[0] - pt4[0]) / withRect)
+        angle = math.acos((pt1[0] - pt4[0]) / withRect) * (180 / math.pi)
+        print(angle)
+
+        if pt4[1] > pt1[1]:
+            angle=-angle
+            angleh=-angleh
+            print("顺时针旋转")
+        else:
+            print("逆时针旋转")
+            
+        self.angleList.append(-1 * angleh)
+
+        height = img.shape[0] # 原始图像高度
+        width = img.shape[1]  # 原始图像宽度
+        #angle > 0 ni angle < 0 shun
+        rotateMat = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1) # 按angle角度旋转图像
+        heightNew = int(width * fabs(sin(radians(angle))) + height * fabs(cos(radians(angle))))
+        widthNew = int(height * fabs(sin(radians(angle))) + width * fabs(cos(radians(angle))))
+
+        rotateMat[0, 2] += (widthNew - width) / 2
+        rotateMat[1, 2] += (heightNew - height) / 2
+        imgRotation = cv2.warpAffine(img, rotateMat, (widthNew, heightNew), borderValue=(255, 255, 255))
+        print("x")
+        global num
+        num = num + 1
+        global args
+        cv2.imwrite(os.path.join(args.output_path,"imgRotation.jpg"),imgRotation)
+
+        # 旋转后图像的四点坐标
+        [[pt1[0]], [pt1[1]]] = np.dot(rotateMat, np.array([[pt1[0]], [pt1[1]], [1]]))
+        [[pt3[0]], [pt3[1]]] = np.dot(rotateMat, np.array([[pt3[0]], [pt3[1]], [1]]))
+        [[pt2[0]], [pt2[1]]] = np.dot(rotateMat, np.array([[pt2[0]], [pt2[1]], [1]]))
+        [[pt4[0]], [pt4[1]]] = np.dot(rotateMat, np.array([[pt4[0]], [pt4[1]], [1]]))
+
+        # 处理反转的情况
+        if pt2[1]>pt4[1]:
+            pt2[1],pt4[1]=pt4[1],pt2[1]
+        if pt1[0]>pt3[0]:
+            pt1[0],pt3[0]=pt3[0],pt1[0]
+        imgOut = imgRotation[int(pt2[1]):int(pt4[1]), int(pt1[0]):int(pt3[0])]
+
+        
+        #cv2.namedWindow('imgOut',cv2.WINDOW_NORMAL)
+        #cv2.imshow("imgOut", imgOut) # 裁减得到的旋转矩形框
+        #cv2.waitKey(30)
+        cv2.imwrite(os.path.join(args.output_path,"imgOut.jpg"),imgOut)
+
+
+        tpl = imgOut.copy()
+        src_path = args.template_path
+        filenames = os.listdir(src_path)
+        minest = 100.0
+        for target1 in filenames:
+            target = cv2.imread(src_path + target1)
+            #tpl = cv.imread("/home/sijia/OCR/templet_image/cai/R.jpg")
+            #cv.imshow("modul", tpl)
+            #cv.imshow("yuan", target)
+            methods = [cv2.TM_SQDIFF_NORMED]#, cv.TM_CCORR_NORMED, cv.TM_CCOEFF_NORMED]
+            th, tw = tpl.shape[:2]
+            for md in methods:
+                result = cv2.matchTemplate(target, tpl, md)
+                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+                if md == cv2.TM_SQDIFF_NORMED:
+                    tl = min_loc
+                    if min_val < minest:
+                        minest = min_val
+                        min_image = target1
+                    print(min_val)
+                else:
+                    tl = max_loc
+                    print(max_val)
+                br = (tl[0] + tw, tl[1] + th)
+                cv2.rectangle(target, tl, br, [0, 0, 0])
+
+        
+
+        self.charname.append(min_image[:-5])
+        self.charnum.append(min_image[-5:-4])
+
+        gray_image = cv2.cvtColor(imgOut.copy(), cv2.COLOR_BGR2GRAY)
+        gray_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+        ret3, th3 = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        return imgRotation # rotated image
+
+    def on_mouse(self,event, x, y, flags, param):
+        print("select the detect area")
+        global point1, point2, img, area
+        img2 = img.copy()
+        if event == cv2.EVENT_LBUTTONDOWN:         #左键点击
+            point1 = (x,y)
+            cv2.circle(img2, point1, 10, (0,255,0), 5)
+            cv2.imshow('image', img2)
+        elif event == cv2.EVENT_MOUSEMOVE and (flags & cv2.EVENT_FLAG_LBUTTON):               #按住左键拖曳
+            cv2.rectangle(img2, point1, (x,y), (255,0,0), 5)
+            cv2.imshow('image', img2)
+        elif event == cv2.EVENT_LBUTTONUP:         #左键释放
+            point2 = (x,y)
+            cv2.rectangle(img2, point1, point2, (0,0,255), 5) 
+            cv2.imshow('image', img2)
+            min_x = min(point1[0],point2[0])     
+            min_y = min(point1[1],point2[1])
+            width = abs(point1[0] - point2[0])
+            height = abs(point1[1] -point2[1])
+            cut_img = img[min_y:min_y+height, min_x:min_x+width]
+            cv2.imwrite('lena3.jpg', cut_img)
+            area = 1
+            print("tf version",tf.__version__)
+
+
+    def resize_image(self,im, max_side_len=2400):
+        '''
+        resize image to a size multiple of 32 which is required by the network
+        :param im: the resized image
+        :param max_side_len: limit of max image size to avoid out of memory in gpu
+        :return: the resized image and the resize ratio
+        '''
+        h, w, _ = im.shape
+
+        resize_w = w
+        resize_h = h
+
+        # limit the max side
+        if max(resize_h, resize_w) > max_side_len:
+            ratio = float(max_side_len) / resize_h if resize_h > resize_w else float(max_side_len) / resize_w
+        else:
+            ratio = 1.
+        resize_h = int(resize_h * ratio)
+        resize_w = int(resize_w * ratio)
+
+        resize_h = resize_h if resize_h % 32 == 0 else (resize_h // 32 - 1) * 32
+        resize_w = resize_w if resize_w % 32 == 0 else (resize_w // 32 - 1) * 32
+        resize_h = max(32, resize_h)
+        resize_w = max(32, resize_w)
+        im = cv2.resize(im, (int(resize_w), int(resize_h)))
+
+        ratio_h = resize_h / float(h)
+        ratio_w = resize_w / float(w)
+
+        return im, (ratio_h, ratio_w)
+
+
+    def detect(self,score_map, geo_map, timer, score_map_thresh=0.8, box_thresh=0.1, nms_thres=0.2):
+        '''
+        restore text boxes from score map and geo map
+        :param score_map:
+        :param geo_map:
+        :param timer:
+        :param score_map_thresh: threshhold for score map
+        :param box_thresh: threshhold for boxes
+        :param nms_thres: threshold for nms
+        :return:
+        '''
+        if len(score_map.shape) == 4:
+            score_map = score_map[0, :, :, 0]
+            geo_map = geo_map[0, :, :, ]
+        # filter the score map
+        xy_text = np.argwhere(score_map > score_map_thresh)
+        # sort the text boxes via the y axis
+        xy_text = xy_text[np.argsort(xy_text[:, 0])]
+        # restore
+        start = time.time()
+        text_box_restored = restore_rectangle(xy_text[:, ::-1]*4, geo_map[xy_text[:, 0], xy_text[:, 1], :]) # N*4*2
+        print('{} text boxes before nms'.format(text_box_restored.shape[0]))
+        #print("zaina")
+        boxes = np.zeros((text_box_restored.shape[0], 9), dtype=np.float32)
+        boxes[:, :8] = text_box_restored.reshape((-1, 8))
+        boxes[:, 8] = score_map[xy_text[:, 0], xy_text[:, 1]]
+        timer['restore'] = time.time() - start
+
+        # nms part
+        start = time.time()
+        # boxes = nms_locality.nms_locality(boxes.astype(np.float64), nms_thres)
+        boxes = lanms.merge_quadrangle_n9(boxes.astype('float32'), nms_thres)
+        timer['nms'] = time.time() - start
+        
+        if boxes.shape[0] == 0:
+            return None, timer
+
+        # here we filter some low score boxes by the average score map, this is different from the orginal paper
+        for i, box in enumerate(boxes):
+            mask = np.zeros_like(score_map, dtype=np.uint8)
+            cv2.fillPoly(mask, box[:8].reshape((-1, 4, 2)).astype(np.int32) // 4, 1)
+            boxes[i, 8] = cv2.mean(score_map, mask)[0]
+        boxes = boxes[boxes[:, 8] > box_thresh]
+
+        return boxes, timer
+
+
+    def sort_poly(self,p):
+        min_axis = np.argmin(np.sum(p, axis=1))
+        p = p[[min_axis, (min_axis+1)%4, (min_axis+2)%4, (min_axis+3)%4]]
+        if abs(p[0, 0] - p[1, 0]) > abs(p[0, 1] - p[1, 1]):
+            return p
+        else:
+            return p[[0, 3, 2, 1]]
+
+    def Iscollect(self):
+        pass
+
+    def detect_char(self,req):
+
+        charObjList = []
+        charName    = []
+        charNum     = []
+        angle       = []
+        res =  detectobjectionSrvResponse.NOT_DETECTED
+
+
+        self.charname.clear()
+        self.charnum.clear()
+        self.poseList.clear()
+        self.angleList.clear()
+
+        imageData = rospy.wait_for_message('/usb_cam/image_raw', Image, timeout=5)
+        try:
+            cv_image = CvBridge().imgmsg_to_cv2(imageData,"bgr8")
+
+        except CvBridgeError as e:
+            print(e)
+
+        global args
+        cv2.imwrite(args.output_path+"cv_image.jpg",cv_image)
+        if req.objectType is detectobjectionSrvRequest.ALL_OBJECT:
+            global img, area, point1, point2
+            img = cv_image.copy()
+            img2 = cv_image.copy()
+            im = cv_image[:, :, ::-1]
+
+            if area == 0:
+                cv2.namedWindow('image')
+                cv2.setMouseCallback('image', self.on_mouse)
+            while True:
+                if(area == 1):
+                    break
+                cv2.imshow('image', im)
+                cv2.waitKey(30)
+            
+            cv2.destroyAllWindows()
+            cv2.waitKey(15)
+            area = 1
+            h, w, _ = im.shape
+            start_time = time.time()
+            im_resized, (ratio_h, ratio_w) = self.resize_image(im)
+            h, w, _ = im.shape
+
+            timer = {'net': 0, 'restore': 0, 'nms': 0}
+            start = time.time()
+
+
+
+            score, geometry = self.sess.run([self.f_score, self.f_geometry], feed_dict={self.input_images: [im_resized]})
+            timer['net'] = time.time() - start
+
+            boxes, timer = self.detect(score_map=score, geo_map=geometry, timer=timer)
+
+            if boxes is not None:
+                boxes = boxes[:, :8].reshape((-1, 4, 2))
+                boxes[:, :, 0] /= ratio_w
+                boxes[:, :, 1] /= ratio_h
+
+            duration = time.time() - start_time
+            print('[timing] {}'.format(duration))
+
+            cv2.rectangle(img2, point1, point2, (255,0,0), 5)
+
+            cv2.imwrite(args.output_path+"img2.jpg",img2)
+
+            boxnum = 0
+            boxdic = {}
+            if boxes is not None:
+                i = 0
+                for box in boxes:
+                    if i > 9:
+                        break
+                    # to avoid submitting errors
+                    box = self.sort_poly(box.astype(np.int32))
+                    if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3]-box[0]) < 5:
+                        continue
+                    ##zai gou xuan de qu yu if()
+                    x0 = box[0,0] >= point1[0] and box[0,0] <= point2[0]
+                    y0 = box[0,1] >= point1[1] and box[0,1] <= point2[1]
+                    x1 = box[1,0] >= point1[0] and box[1,0] <= point2[0]
+                    y1 = box[1,1] >= point1[1] and box[1,1] <= point2[1]
+                    x2 = box[2,0] >= point1[0] and box[2,0] <= point2[0]
+                    y2 = box[2,1] >= point1[1] and box[2,1] <= point2[1]
+                    x3 = box[3,0] >= point1[0] and box[3,0] <= point2[0]
+                    y3 = box[3,1] >= point1[1] and box[3,1] <= point2[1]
+                    if x0 and y0 and x1 and y1 and x2 and y2 and x3 and y3:
+                        print("box", box[0, 0], box[0, 1], box[1, 0], box[1, 1], box[2, 0], box[2, 1], box[3, 0], box[3, 1])
+                        img2 = cv_image.copy()
+
+                        charPose = Pose()
+                        charPose.position.x = int(box[3,0]+(box[1,0]-box[3,0])/2)
+                        charPose.position.y = int(box[0,1]+(box[2,1]-box[0,1])/2)
+                        self.poseList.append(charPose)
+
+                        cv2.circle(img2, tuple(box[0]), 3, (0,255,0), 2)
+                        cv2.circle(img2, tuple(box[1]), 3, (255,255,0), 2)
+                        cv2.circle(img2, tuple(box[2]), 3, (255,255,255), 2)
+                        cv2.circle(img2, (charPose.position.x,charPose.position.y), 3, (0,255,0), 2)
+
+                        cv2.imwrite(args.output_path+"img3.jpg",img2)
+
+                        ###################
+                        boxdic['box%d'%boxnum] = [[box[0, 0], box[0, 1]],box[1, 0], box[1, 1],[box[2, 0], box[2, 1]],[box[3, 0], box[3, 1]]]
+                        ####################
+                        image_happy = cv_image.copy()
+                        self.rotate(image_happy,box[1],box[2],box[3],box[0])
+
+
+
+
+                        cv2.polylines(im[:, :, ::-1], [box.astype(np.int32).reshape((-1, 1, 2))], True, color=(255, 255, 0), thickness=1)
+                    i = i + 1
+                cv2.imwrite(args.output_path+"detection.jpg",im[:, :, ::-1])
+
+        charObjList = self.poseList
+        charName    = self.charname
+        charNum     = self.charnum
+        angle       = self.angleList
+
+        
+        return detectobjectionSrvResponse(res,charObjList,charName,charNum,angle)
+
+
+if __name__ == '__main__':
+
+    rospy.init_node('Object_Detect_ocr')
+
+    parser = argparse.ArgumentParser(description='manual to this script')
+    parser.add_argument('--model_path', type=str, default = None)
+    parser.add_argument('--template_path', type=str, default = None)
+    parser.add_argument('--output_path', type=str, default = None)
+    global args
+    args = parser.parse_args()
+
+    detect = TOD()
+    ser = rospy.Service('probot_detect_ocr',detectobjectionSrv,detect.detect_char)
+    rospy.loginfo("Server is ready to detect.")
+    rospy.spin()
+
---
+++ probot_vision_pick/probot_pick_place/scripts/probot_sorting_ocr .py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# Copyright 2019 Wuhan PS-Micro Technology Co., Itd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rospy, sys
+import moveit_commander
+import time
+
+
+
+from moveit_msgs.msg import RobotTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+
+from geometry_msgs.msg import PoseStamped, Pose
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from moveit_commander import RobotCommander, MoveGroupCommander, PlanningSceneInterface
+from probot_ocr.srv import *
+from probot_msgs.msg import SetOutputIO
+
+
+putlocation={'H':(0.35374,0.12474),'E':(0.35374,0.05321),'L':(0.35374,-0.02449),'LL':(0.35374,-0.0998),'O':(0.35374,-0.17143),'R':(0.23204,0.12495),'OO':(0.23204,0.046619),'S':(0.23204,-0.026839),'I':(0.23204,-0.10482)}
+
+needangle = {0:0,1:1.57,2:-3.14,3:-1.57}
+
+class ProbotSorting:
+    def __init__(self):
+        moveit_commander.roscpp_initialize(sys.argv)
+        self.arm = moveit_commander.MoveGroupCommander('manipulator')
+        self.end_effector_link = self.arm.get_end_effector_link()
+        self.reference_frame = 'base_link'
+        self.arm.set_pose_reference_frame(self.reference_frame)
+        self.arm.allow_replanning(True)
+        self.arm.set_goal_position_tolerance(0.001)
+        self.arm.set_goal_orientation_tolerance(0.01)
+
+        # 初始化场景对象
+        self.scene = PlanningSceneInterface()
+        rospy.sleep(1)
+
+        # Initialize IO
+        self.ioPub = rospy.Publisher('probot_set_output_io', SetOutputIO, queue_size=1)
+
+        self.arm.set_named_target('home')
+        self.arm.go()
+        rospy.sleep(1)
+
+        # 移除场景中之前运行残留的物体
+        #scene.remove_attached_object(end_effector_link, 'tool')
+        self.scene.remove_world_object('table') 
+        self.scene.remove_world_object('target')
+
+        # 设置桌面的高度
+        self.table_ground = 0.05
+        # 设置table和tool的三维尺寸
+        self.table_size = [0.04, 0.04, 1.2]
+        # 将table加入场景当中
+        self.table_pose = PoseStamped()
+        self.table_pose.header.frame_id = 'base_link'
+        self.table_pose.pose.position.x = -0.2
+        self.table_pose.pose.position.y = 0.2
+        self.table_pose.pose.position.z =  0.2
+        self.table_pose.pose.orientation.w = 1.0
+        self.scene.add_box('table', self.table_pose, self.table_size)
+        
+        rospy.sleep(2)  
+        # 更新当前的位姿
+        self.arm.set_start_state_to_current_state()
+        #########
+
+    def moveToHome(self):
+        self.arm.set_named_target('home')
+        self.arm.go()
+        rospy.sleep(1)
+
+    def moveTo(self, x, y, z):
+        target_pose = PoseStamped()
+        target_pose.header.frame_id = self.reference_frame
+        target_pose.header.stamp = rospy.Time.now()
+        target_pose.pose.position.x = x
+        target_pose.pose.position.y = y
+        target_pose.pose.position.z = z
+        target_pose.pose.orientation.x = 0.70692
+        target_pose.pose.orientation.y = 0
+        target_pose.pose.orientation.z = 0
+        target_pose.pose.orientation.w = 0.70729
+
+        self.arm.set_start_state_to_current_state()
+        self.arm.set_pose_target(target_pose, self.end_effector_link)
+
+        traj = self.arm.plan()
+        self.arm.execute(traj)
+
+    def moveSingleJoint(self, index, value):
+        group_variable_values = self.arm.get_current_joint_values()
+        group_variable_values[index] = value
+        self.arm.set_joint_value_target(group_variable_values)
+        traj = self.arm.plan()
+        self.arm.execute(traj)
+
+    def pick(self, x, y):
+        self.moveTo(x, y, 0.28)
+
+        ioOutput0 = SetOutputIO()
+        ioOutput0.ioNumber = 1
+        ioOutput0.status = SetOutputIO.IO_HIGH
+        self.ioPub.publish(ioOutput0)
+        rospy.sleep(1)
+
+
+    def tai(self, x, y):
+
+        self.moveTo(x, y, 0.35)
+        ioOutput0 = SetOutputIO()
+        ioOutput0.ioNumber = 1
+        ioOutput0.status = SetOutputIO.IO_HIGH
+        self.ioPub.publish(ioOutput0)
+        rospy.sleep(1)
+
+
+
+    def place(self, x, y):
+
+        group_variable_values_qian = self.arm.get_current_joint_values()
+        self.moveTo(x,y,0.28)
+        rospy.sleep(0.2)
+        group_variable_values_hou = self.arm.get_current_joint_values()
+        self.ro1(group_variable_values_qian[5],5)
+
+
+        deta = group_variable_values_qian[0] - group_variable_values_hou[0]
+        a = group_variable_values_hou[5] - deta
+        self.ro(a,5)
+        
+        self.ro(1.57,5)
+        ioOutput0 = SetOutputIO()
+        ioOutput0.ioNumber = 1
+        ioOutput0.status = SetOutputIO.IO_LOW
+        self.ioPub.publish(ioOutput0)
+        rospy.sleep(1)
+
+
+    def shutdown(self):
+        print "The sorting is shutting down"
+        moveit_commander.roscpp_shutdown()
+        moveit_commander.os._exit(0)
+
+    def qiren(self):
+        group_variable_values = self.arm.get_current_joint_values()
+        print group_variable_values[0]
+        deta = group_variable_values[0] - 7.272198854479939e-05
+        a = group_variable_values[5] - deta
+        print a
+        if a > 3.14:
+            a = -(3.1415926*2 - a)
+        if a < -3.14:
+            a = (3.1415926*2 + a)
+        joint_positions = [7.272198854479939e-05,group_variable_values[1],group_variable_values[2],group_variable_values[3],group_variable_values[4],a]
+        self.arm.set_joint_value_target(joint_positions)
+                 
+        # 控制机械臂完成运动
+        self.arm.go()
+        rospy.sleep(1)
+    def qiren2(self):
+        group_variable_values = self.arm.get_current_joint_values()
+
+        joint_positions = [1.57, group_variable_values[1],group_variable_values[2],group_variable_values[3],group_variable_values[4],group_variable_values[5]]
+        self.arm.set_joint_value_target(joint_positions)
+                 
+        # 控制机械臂完成运动
+        self.arm.go()
+        rospy.sleep(1)
+
+    def ro1(self,angle,num):
+        group_variable_values = self.arm.get_current_joint_values()
+        #print group_variable_values
+        group_variable_values[num] = angle
+        if group_variable_values[num] > 3.14:
+            group_variable_values[num] = -(3.1415926*2 - group_variable_values[num])
+        if group_variable_values[num] < -3.14:
+            group_variable_values[num] = (3.1415926*2 + group_variable_values[num])
+        self.arm.set_joint_value_target(group_variable_values)
+        traj = self.arm.plan()
+        
+        # 按照规划的运动路径控制机械臂运动
+        self.arm.execute(traj)
+        rospy.sleep(1) 
+
+    def ro(self,angle,num):
+        group_variable_values = self.arm.get_current_joint_values()
+        #print group_variable_values
+        group_variable_values[num] = group_variable_values[num]+angle
+        if group_variable_values[num] > 3.14:
+            group_variable_values[num] = -(3.1415926*2 - group_variable_values[num])
+        if group_variable_values[num] < -3.14:
+            group_variable_values[num] = (3.1415926*2 + group_variable_values[num])
+        self.arm.set_joint_value_target(group_variable_values)
+        traj = self.arm.plan()
+        
+        # 按照规划的运动路径控制机械臂运动
+        self.arm.execute(traj)
+        rospy.sleep(1) 
+
+if __name__=="__main__":
+    
+    time.sleep(5)
+    rospy.init_node('probot_sorting')
+    rate = rospy.Rate(10)
+
+    rospy.loginfo("probot sorting start...")
+    sort = ProbotSorting()
+
+    while not rospy.is_shutdown():
+        rospy.wait_for_service('probot_detect_ocr')
+        try:
+            object_detect_service = rospy.ServiceProxy('probot_detect_ocr',detectobjectionSrv)
+            response = object_detect_service(detectobjectionSrvRequest.ALL_OBJECT)
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+
+        if response.result is not detectobjectionSrvResponse.SUCCESS:
+            rospy.loginfo("No objects detected , waiting detecting.....")
+            rate.sleep()
+            continue
+
+        rospy.loginfo("Get object position, Start pick and place")
+
+        print response.charObjList
+        print response.charName
+        print response.charNum
+        print response.angle
+
+        L = len(response.charObjList)
+        for i in range(0,L):
+            sort.moveToHome()
+            sort.ro(1.0,0)
+            sort.pick(response.charObjList[i][0],response.charObjList[i][1])
+            sort.tai(response.charObjList[i][0],response.charObjList[i][1])
+            sort.ro(response.angle[i],5)
+            sort.ro(needangle[response.charNum[i]],5)
+            sort.qiren()
+
+            sort.place(putlocation[response.charName[i]][0],putlocation[response.charName[i]][1])
+
+            sort.moveToHome()
+
+        rate.sleep()
+
+    sort.moveToHome()
+    sort.shutdown()
+
+
+
+
